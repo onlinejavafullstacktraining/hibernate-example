97b78ee7bdc998fcbb0a978f6d2e98013007f5f6
Javatechnology <javatechnology.github@gmail.com>
2022-08-11 11:50:21 +0530

    Hibernate properties file added

--- src/main/java/com/hibernate/core/HibernateMain.java
+++ src/main/java/com/hibernate/core/HibernateMain.java
@@ -9,11 +9,18 @@ public class HibernateMain {
     public static void main(String[] args) {
 
         /* Connecting Database through to hibernate */
-        Configuration configuration=new Configuration().configure("hibernate.cfg.xml");
+
+        /*Removed the hibernate.cfg.xml file from the Configuration class
+         using the hibernate.properties file
+        */
+        Configuration configuration = new Configuration()
+                .addResource("Message.hbm.xml");
+        //      .addClass(Message.class);
+        // when we use addClass method the hbm file should be in available in same folder.
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
-        Message message = new Message("Log4j Properties");
+        Message message = new Message("Hibernate Properties");
         session.save(message);
         tx.commit();
         session.close();
